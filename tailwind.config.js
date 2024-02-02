1991178a747bdada7ce72ebe23e8810db2479af7
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2024-02-02 20:11:25 -0300

    esboço do header, icon e nome

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+     colors: {
+       white: '#ffffff',
+       lightgray: '#f3f5f8',
+       gray: '#acacac',
+       black: '#2E2D37',
+       red: '#f3123c',
+       aquagray: '#d6d6dc'
+     }
     },
   },
   plugins: [],
