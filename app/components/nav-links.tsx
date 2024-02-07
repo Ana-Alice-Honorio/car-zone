278382d06bd9554c2d155c7034345a549ffba3af
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2024-02-06 22:58:43 -0300

    add clsx e fix de cor da nav

--- app/components/nav-links.tsx
+++ app/components/nav-links.tsx
@@ -1,9 +1,14 @@
+'use client';
+
 import {
     UserGroupIcon,
     HomeIcon,
     DocumentDuplicateIcon,
   } from '@heroicons/react/24/outline';
-  
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
   const links = [
     { name: 'Home', href: '/home', icon: HomeIcon },
     {
@@ -15,19 +20,25 @@ import {
   ];
   
   export default function NavLinks() {
+    const pathname = usePathname();
     return (
       <>
         {links.map((link) => {
           const LinkIcon = link.icon;
           return (
-            <a
+            <Link
               key={link.name}
               href={link.href}
-              className="flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-aquagray hover:text-black md:flex-none md:justify-start md:p-2 md:px-3"
+              className={clsx(
+                'flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-aquagray hover:text-black md:flex-none md:justify-start md:p-2 md:px-3',
+                {
+                  'bg-aquagray text-black': pathname === link.href,
+                },
+              )}
             >
               <LinkIcon className="w-6" />
               <p className="hidden md:block">{link.name}</p>
-            </a>
+            </Link>
           );
         })}
       </>
